2e000c91b80af3e63d5ed14b8830210414d11735
Darina Sivachenko <sdaryna@ukr.net>
2021-12-06 00:09:20 +0200

    react frontend

---
+++ react/src/Get.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+
+
+class Get extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {product: {}, id: {}}
+    }
+
+    componentDidMount(){
+        console.log("Get app mounted")
+    }
+
+    componentDidUpdate(){
+        console.log("Get app updated")
+    }
+
+    onIdChange = (e) => {
+        this.setState({id:e.target.value})
+    }
+
+    getProduct(){
+        const axios = require("axios")
+        axios.get("http://localhost:8080/api/products/" + this.state.id)
+        .then(res=>{
+            this.setState({product:res.data[0]})
+        })
+    }
+
+    render(){
+        return (
+            <div>
+              Enter product id:<input onChange={this.onIdChange} />
+              <button onClick={this.getProduct.bind(this)}>Get Product</button>
+              <br/>
+              Name: {this.state.product.name}
+              <br/>
+              Description: {this.state.product.desc}
+              <br/>
+              Price: {this.state.product.price}
+            </div>
+          );
+    }
+}
+
+export default Get;
