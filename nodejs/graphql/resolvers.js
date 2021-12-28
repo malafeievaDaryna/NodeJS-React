a318e75cbba78b9671ff020117649456056d5607
Yevhen <ymalafeiev@luxoft.com>
2021-12-28 18:55:28 +0200

    mutation for adding products

--- nodejs/graphql/resolvers.js
+++ nodejs/graphql/resolvers.js
@@ -13,4 +13,20 @@ const Query = {
     }
 };
 
-module.exports = { Query };
+const Mutation = {
+    createProduct: async (root, {input}) => {
+        const new_id = await product.countDocuments({}).exec();
+        const new_item = {...input, new_id };
+        console.log("GraphQL creats " + JSON.stringify(new_item))
+
+        return await product.create(new_item).then( result => {
+            console.log("GraphQL suceeded " + result);
+            return true;
+        }).catch (err => {
+            console.log("GraphQL failed " + err);
+            return false;
+        });
+    }
+};
+
+module.exports = { Query, Mutation };
